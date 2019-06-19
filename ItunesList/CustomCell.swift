734e3ad6b93d813ac7d05613c8f4ab5b2da622de
Balaji Peddaiahgari <Balaji_Peddaiahgari@gap.com>
2019-06-18 21:19:34 -0700

    Displaying Apple music Itunes list media

--- ItunesList/CustomCell.swift
+++ ItunesList/CustomCell.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+    var name: String?
+    var mainImage: UIImage?
+    var type: String?
+    
+    var nameView: UITextView = {
+        var textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        return textView
+    }()
+    
+    var mainImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var typeView: UITextView = {
+        var textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        return textView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubview(mainImageView)
+        self.addSubview(nameView)
+        self.addSubview(typeView)
+        
+        mainImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        mainImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+        mainImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        mainImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        nameView.leftAnchor.constraint(equalTo: self.mainImageView.rightAnchor, constant: 10).isActive = true
+        nameView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        nameView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -25).isActive = true
+        nameView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        
+        typeView.leftAnchor.constraint(equalTo: self.mainImageView.rightAnchor, constant: 10).isActive = true
+        typeView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        typeView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        typeView.topAnchor.constraint(equalTo: self.nameView.bottomAnchor, constant: -10).isActive = true
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let name = name {
+            let attrs = [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 15)]
+            let boldString = NSMutableAttributedString(string: name, attributes:attrs)
+            nameView.attributedText = boldString
+        }
+        if let image = mainImage {
+            mainImageView.image = image
+        }
+        if let type = type {
+            let attrs = [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 11), NSMutableAttributedString.Key.foregroundColor : UIColor.lightGray]
+            let boldString = NSMutableAttributedString(string: type, attributes:attrs)
+            typeView.attributedText = boldString
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
--- ItunesList/ItunesListViewController.swift
+++ ItunesList/ItunesListViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ItunesListViewController: UITableViewController {
+    
+    private var nameArray = [String]()
+    private var typeArray = [String]()
+    private var imgURLArray = [String]()
+    private let urlString = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.json"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(CustomCell.self, forCellReuseIdentifier: "custom")
+        downloadJsonWithURL(named: urlString)
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 200
     }
+    
+    func downloadJsonWithURL(named: String) {
+        let url = URL(string: named)
+        URLSession.shared.dataTask(with: url!, completionHandler: { [weak self] (data, response, error) -> Void in
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                if let feedObject = jsonObj.value(forKey: "feed") as? NSDictionary, let resultArray = feedObject.value(forKey: "results") as? NSArray {
+                    for result in resultArray {
+                        if let result = result as? NSDictionary {
+                            if let name = result.value(forKey: "name") {
+                                self?.nameArray.append(name as! String)
+                            }
+                            if let kind = result.value(forKey: "kind") {
+                                self?.typeArray.append(kind as! String)
+                            }
+                            if let imageURL = result.value(forKey: "artworkUrl100") {
+                                self?.imgURLArray.append(imageURL as! String)
+                            }
+                        }
+                    }
+                }
+                OperationQueue.main.addOperation({
+                    self?.tableView.reloadData()
+                })
+            }
+        }).resume()
+    }
+}
 
-
+extension ItunesListViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "custom") as! CustomCell
+        cell.name = nameArray[indexPath.row]
+        cell.type = typeArray[indexPath.row]
+        let imageURL = URL(string: imgURLArray[indexPath.row])
+        cell.mainImageView.load(url: imageURL!)
+        cell.layoutSubviews()
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
 }
 
